1bf8c60755e9a71797c1e13b9b688daf9e15b899
Matt Claypotch <thepotch@gmail.com>
2014-10-09 14:49:32 -0700

    get right version of slimerjs

--- test/install-slimer.sh
+++ test/install-slimer.sh
@@ -1,5 +1,5 @@
 echo 'Installing Slimer'
-wget http://download.slimerjs.org/v0.8/0.8.5/slimerjs-0.8.5.zip -O slimerjs.zip
+wget http://download.slimerjs.org/releases/0.9.3/slimerjs-0.9.3.zip -O slimerjs.zip
 unzip slimerjs.zip -d slimer-temp
 mv slimer-temp/slimer* slimerjs/
 rm -r slimer-temp
