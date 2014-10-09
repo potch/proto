9b9ef8bf733ccbca9ddeaba74958f86b40ebf8cd
Matt Claypotch <thepotch@gmail.com>
2014-10-09 15:42:23 -0700

    added retry mechanism

--- test/capture.js
+++ test/capture.js
@@ -1,15 +1,31 @@
 var page = require("webpage").create();
 
-page.open("http://localhost:3001/test", function (status) {
-  if (status == "success") {
-    setTimeout(function () {
-      page.render('screenshots/fx28.png');
-      page.close();
-      phantom.exit();
-    }, 2000);
-  } else {
-    console.log("Sorry, the page is not loaded");
-    console.error(status);
-    phantom.exit(1);
+var tries = 0;
+
+function tryLoad(url, cb) {
+  function tryAgain() {
+    tries++;
+    page.open(url, function (status) {
+      if (status === 'success') {
+        cb();
+      } else {
+        console.log('failed attempt ' + tries);
+        if (tries > 5) {
+          console.log('loading failed');
+          phantom.exit(1);
+        } else {
+          setTimeout(tryAgain, 1000);
+        }
+      }
+    });
   }
+  tryAgain();
+}
+
+tryLoad("http://localhost:3001/test", function () {
+  setTimeout(function () {
+    page.render('screenshots/fx28.png');
+    page.close();
+    phantom.exit();
+  }, 2000);
 });
