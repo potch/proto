201a315871197c4a90045104d79497f7e0a34cd2
Matt Claypotch <thepotch@gmail.com>
2014-10-09 14:45:33 -0700

    set git right

--- test/screenshot.sh
+++ test/screenshot.sh
@@ -1,5 +1,8 @@
 gulp connect&
 mkdir screenshots
 slimerjs test/capture.js
+
+git config --global user.email "brick@travis.biz"
+git config --global user.name "Travis"
 git add screenshots -f
 git commit -m "updated screenshots $(date)"
