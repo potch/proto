c6283dfac0170ee4c008bd8c3e1ed5063e0e36e3
Matt Claypotch <thepotch@gmail.com>
2014-10-09 15:50:00 -0700

    make git output verboser

--- test/gh-pages.sh
+++ test/gh-pages.sh
@@ -1,4 +1,4 @@
 echo 'updating gh-pages'
 git remote rm origin
 git remote add origin https://potch:$GITHUB_TOKEN@github.com/potch/proto.git
-git push -fq origin master:gh-pages
+git push -f origin master:gh-pages
